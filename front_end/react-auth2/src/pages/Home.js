6cb3f257938f381047065abf682bc1706ce16d6a
zennon-sml <zennonsampaioml@gmail.com>
2023-01-16 09:11:57 -0300

    react link is not working

--- front_end/react-auth2/src/pages/Home.js
+++ front_end/react-auth2/src/pages/Home.js
@@ -1,9 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom"
 
 const Home = () =>{
     return (
         <div>
             Home
+            <Link to="/">Home</Link>
         </div>
     )
 }
