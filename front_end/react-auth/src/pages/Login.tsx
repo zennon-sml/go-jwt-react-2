77465bb23fff9345074edff24f1364fa6a51d270
zennon-sml <zennonsampaioml@gmail.com>
2023-01-19 08:37:14 -0300

    commit before configuring git on workspace

--- front_end/react-auth/src/pages/Login.tsx
+++ front_end/react-auth/src/pages/Login.tsx
@@ -8,11 +8,11 @@ const Login = () => {
         <img className="mb-4" src="https://github.com/zennon-sml/prog_amador/blob/main/PikPng.com_pokemon-starters-png_1677143.png?raw=true" alt="" width="300" height="100" />
         <h1 className="h3 mb-3 fw-normal">LOGIN</h1>
         <div className="form-floating">
-        <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" />
+        <input type="email" className="form-control" placeholder="name@example.com" />
         <label >Email address</label>
         </div>
         <div className="form-floating">
-        <input type="password" className="form-control" id="floatingPassword" placeholder="Password" />
+        <input type="password" className="form-control" placeholder="Password" />
         <label>Password</label>
         </div>
         <button className="w-100 btn btn-lg btn-primary" type="submit">login</button>
